6433437c9b9a24a13aacc3b958d379780d3c67d7
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-07-14 14:50:02 +0000

    finish project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,15 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>{Math.floor(props.seconds / 100000)%10}</h1>
+			<h1>{Math.floor(props.seconds / 10000)%10}</h1>
+			<h1>{Math.floor(props.seconds / 1000)%10}</h1>
+			<h1>{Math.floor(props.seconds / 100)%10}</h1>
+			<h1>{Math.floor(props.seconds / 10)%10}</h1>
+			<h1>{Math.floor(props.seconds / 1)%10}</h1>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,9 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 0
+setInterval(()=>{
+    seconds++
+    ReactDOM.render(<Home seconds ={seconds} />, document.querySelector("#app"));
+})
+
